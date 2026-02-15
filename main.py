316c7aaa7c06c824bf101c7b1b0d57478990a0ab
kartikeyamani <kartikeyamani0724@gmail.com>
2026-02-15 00:49:03 -0700

    Project running in local

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.entity.config_entity import (DataIngestionConfig,
                                                   TrainingPipelineConfig,
                                                   DataValidationConfig,
-                                                  DataTransformationConfig)
+                                                  DataTransformationConfig,
+                                                  ModelTrainerConfig)
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.model_trainer import ModelTrainer
 
 
 
@@ -33,5 +35,15 @@ if __name__=="__main__":
         data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
+
+        logging.info("Starting Model Trainer")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config)
+
+        model_trainer=ModelTrainer(model_trainer_config,data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        print(model_trainer_artifact)
+
+        
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/utils/main_utils/utils.py
+++ networksecurity/utils/main_utils/utils.py
@@ -6,6 +6,9 @@ import yaml
 import numpy as np
 import pickle
 
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 
 def read_yaml_file(file_path:str)->dict:
     try:
@@ -39,6 +42,53 @@ def save_object(filepath,obj):
         os.makedirs(dir_path,exist_ok=True)
         with open(filepath,"wb") as fileobj:
             pickle.dump(obj,fileobj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+def load_object(file_path:str)->object:
+    try:
+        if not os.path.exists(file_path):
+            raise Exception(f"The file path {file_path} does not exist")
+        with open(file_path,"rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def load_numpy_array(file_path:str)->np.array:
+    try:
+        with open(file_path,"rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+
+def evaulate_models(X_train,y_train,X_test,y_test,models,param):
+    try:
+        report={}
+        for i in range(len(list(models))):
+            model=list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs=GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
     except Exception as e:
         raise NetworkSecurityException(e,sys)
 
